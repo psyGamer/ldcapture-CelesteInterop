8907477bf8f048a57f193e3ab718afc3046bc1a6
psyGamer <psygamer21@gmail.com>
2023-04-15 15:14:58 +0200

    Initial commit

---
+++ LDCaptureModule.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace Celeste.Mod.ldcapture;
+
+public class LDCaptureModule : EverestModule
+{
+    public override void Load()
+    { }
+
+    public override void Unload()
+    { }
+
+    // Use "fmod" as library name since it should exist and if ldcapture is loaded, it should be intercepted by it anyway.
+    // A bit hacky, but it works.
+    [DllImport("fmod")]
+    private static extern void ldcapture_StartRecording();
+    [DllImport("fmod")]
+    private static extern void ldcapture_StopRecording();
+    [DllImport("fmod")]
+    private static extern void ldcapture_StopRecordingAfter(int extensionFrames);
+    [DllImport("fmod")]
+    private static extern void ldcapture_ReloadConfig();
+
+    [Command("start_recording", "Starts the ldcapture recording")]
+    private static void CmdStartRecording()
+    {
+        try
+        {
+            ldcapture_StartRecording();
+            Engine.Commands.Log("Successfully started recording.", Color.LightBlue);
+        }
+        catch (EntryPointNotFoundException)
+        {
+            Engine.Commands.Log("ldcapture is not loaded! Please start Celeste with ldcapture. More information is found on the README.", Color.Red);
+        }
+    }
+
+    [Command("stop_recording", "Stops the ldcapture recording. If specified, extensionFrames frames after this call.")]
+    private static void CmdStopRecording(int extensionFrames = 0)
+    {
+        try
+        {
+            if (extensionFrames == 0)
+                ldcapture_StopRecording();
+            else
+                ldcapture_StopRecordingAfter(extensionFrames);
+            Engine.Commands.Log("Successfully stopped recording.", Color.LightBlue);
+        }
+        catch (EntryPointNotFoundException)
+        {
+            Engine.Commands.Log("ldcapture is not loaded! Please start Celeste with ldcapture. More information is found on the README.", Color.Red);
+        }
+    }
+
+    [Command("reload_config", "Reloads the ldcapture config.")]
+    private static void CmdReloadConfig(int extensionFrames = 0)
+    {
+        try
+        {
+            ldcapture_ReloadConfig();
+            Engine.Commands.Log("Successfully reloaded config.", Color.LightBlue);
+        }
+        catch (EntryPointNotFoundException)
+        {
+            Engine.Commands.Log("ldcapture is not loaded! Please start Celeste with ldcapture. More information is found on the README.", Color.Red);
+        }
+    }
+}
